c9e51b0152f5a4f8621f1185f6e5abac960935ed
Tomasz <you@example.com>
2020-05-01 23:37:48 +0200

    add simple gardens page

--- login/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-from django.template import loader
-
-# Create your views here.
-def index(request):
-    template = loader.get_template('.\login\index.html')
-    context = {}
-    return HttpResponse(template.render(context, request))
