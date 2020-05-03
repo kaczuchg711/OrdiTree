cc3ce685dd8df338335f39d7969488f12057cd95
Tomasz <you@example.com>
2020-05-03 17:04:57 +0200

    xd

--- login/views.py
+++ login/views.py
@@ -1,17 +1,8 @@
 from django.http import HttpResponse
 from django.template import loader
-from django.shortcuts import render
-
 
 # Create your views here.
 def index(request):
     template = loader.get_template('.\login\index.html')
     context = {}
     return HttpResponse(template.render(context, request))
-
-
-# Create your views here.
-def LoginPage(request):
-    template = "LoginPage.html"
-    context = {}
-    return render(request
