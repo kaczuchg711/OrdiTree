5265f7263e03abefd4b90b564984f04e93a827d8
Artur <artur.halat97@gmail.com>
2020-05-11 11:18:00 +0200

    Users login + logout page

--- OrdiTree/urls.py
+++ OrdiTree/urls.py
@@ -15,12 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.contrib.auth import views as auth_views
 from gardens.views import show_gardens
-from login import views
 
 urlpatterns = [
-    path('', views.LoginPage, name='login'),
-    path('login/',include('login.urls')),
+    #path('', views.LoginPage, name='login'),
+    path('login/',auth_views.LoginView.as_view(),name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
     path('admin/', admin.site.urls),
     path('gardens/', show_gardens),
 ]
