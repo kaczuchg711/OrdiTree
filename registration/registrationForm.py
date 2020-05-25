a4efc5d242454602892ae08040680a99c1236e85
Jakub Grodny <kuba.g6@gmail.com>
2020-05-25 15:53:14 +0200

    Registration form complete

--- registration/registrationForm.py
+++ registration/registrationForm.py
@@ -5,8 +5,8 @@ from django.contrib.auth.models import User
 
 class registrationForm(UserCreationForm):
 
-    first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
-    last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
+    first_name = forms.CharField(max_length=30, required=True, help_text='Required.')
+    last_name = forms.CharField(max_length=30, required=True, help_text='Required.')
     email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
 
     class Meta:
