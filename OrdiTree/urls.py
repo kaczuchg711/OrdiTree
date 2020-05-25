9b1bd1e95534b4933eeeb1bda1f203669910bb6b
Artur <artur.halat97@gmail.com>
2020-05-25 14:06:25 +0200

    fix login for /gardens

--- OrdiTree/urls.py
+++ OrdiTree/urls.py
@@ -20,8 +20,6 @@ from gardens.views import show_gardens
 from gardens.views import show_panel
 
 urlpatterns = [
-    #path('', views.LoginPage, name='login'),
-    path('login/',auth_views.LoginView.as_view(),name='login'),
     path('',auth_views.LoginView.as_view(),name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
     path('admin/', admin.site.urls),
