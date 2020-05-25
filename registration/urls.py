c61999acf8aad692aea0eb695f0a19b3bc94655d
Jakub Grodny <kuba.g6@gmail.com>
2020-05-25 13:54:30 +0200

    zmiana koncepcji

--- registration/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-from .views import Registration
-
-urlpatterns=[
-    path('', Registration, name='Registration'),
-    path('',views.index, name='index'),
-]
