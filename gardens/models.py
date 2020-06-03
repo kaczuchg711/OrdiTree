b532f3974cd3a4561db5716e006f4106b9f57c13
Artur <artur.halat97@gmail.com>
2020-06-04 01:32:38 +0200

    start working on plants

--- gardens/models.py
+++ gardens/models.py
@@ -4,5 +4,5 @@ from django.db import models
 # Create your models here.
 class Garden(models.Model):
     name = models.CharField(max_length=30)
-    id_user = models.IntegerField(default=0)
+    id_user = models.IntegerField(default=300)
 
