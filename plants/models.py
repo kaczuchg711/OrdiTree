9d47d3cca8f8b6b153e800d7b503a4e1328f0ba4
Tomek <kaczatom4@gmail.com>
2020-06-08 14:18:48 +0200

    yes

--- plants/models.py
+++ plants/models.py
@@ -8,7 +8,7 @@ from gardens.models import Garden
 class Plant(models.Model):
     name = models.CharField(max_length=30)
     description = models.CharField(max_length=300)
-    # temperature = models.IntegerField(default=20)
+    temperature = models.IntegerField(default=20)
     # manuring_date = models.DateField(default="2000-11-11")
     # this create MM table separate from Plant
     Garden = models.ManyToManyField(Garden)
