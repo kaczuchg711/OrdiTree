d42dcffe3cc4514fb3cb3e318f5569d7041517cc
Tomek <kaczatom4@gmail.com>
2020-06-09 23:17:06 +0200

    start making plants from db

--- gardens/models.py
+++ gardens/models.py
@@ -1,8 +1,7 @@
 # coding=utf-8
 from django.db import models
-
 # Create your models here.
 class Garden(models.Model):
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=29)
     id_user = models.IntegerField()
 
--- plants/views.py
+++ plants/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+from plants.models import Plant
 
 def show_plants(request, *args, **kwargs):
-    return render(request, "plants/plants.html")
+
+    # how to get this 29 ?
+    plants = Plant.objects.filter(Garden=29)
+
+    context = {
+        'user_plants' : plants
+    }
+
+    return render(request, "plants/plants.html", context)
